884d26a5de3a135e228e0fa23961fb128e8782e8
officialhickel <officialhickel@gmail.com>
2020-01-21 14:22:33 -0800

    Changes files from text to jpg

--- EditAstronautPhotoNames.py
+++ EditAstronautPhotoNames.py
@@ -3,4 +3,4 @@ import os
 for file in os.listdir("./astronautFaces/"):
     filename = os.fsdecode(file)
     print(filename)
-    os.rename(r'./astronautFaces/'+filename, r'./astronautFaces/'+filename.split('_')[0]+' '+filename.split('_')[1])
+    os.rename(r'./astronautFaces/'+filename, r'./astronautFaces/'+filename+'.jpg')
